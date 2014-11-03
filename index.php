53038707be9257e0fe7c7422d69b8efc03d5cc09
Tim Whitacre <tim@timw.co>
2014-11-02 21:00:17 -0500

    Changed description

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 
   <section id="intro">
-    <p>Below you'll find some of my thoughts.</p>
-    <p>Find more about me on <a href="http://timw.co">my personal site.</a></p>
+    <p>Here are some of my thoughts. Read more about me <a href="http://timw.co">here</a></p>
   </section>
 
   <section id="content">
