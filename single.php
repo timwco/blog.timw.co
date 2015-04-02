ee02133134c1d53d8b46745c6b5d4b8d37ef9d71
Tim Whitacre <tim@timw.co>
2015-04-02 00:20:02 -0400

    Updated styles

--- single.php
+++ single.php
@@ -8,7 +8,7 @@
           <h1 class="post-title"><?php the_title(); ?></h1>
           <div class="post-content"><?php the_content(); ?></div>
           <time class="post-date">
-            <i class="typcn typcn-calendar"></i> <a href="<?php the_permalink(); ?>"><?php the_time('F j, Y') ?></a>
+            <i class="typcn typcn-calendar-outline"></i> <a href="<?php the_permalink(); ?>"><?php the_time('F j, Y') ?></a>
             <i class="typcn typcn-folder"></i> <?php the_category(', '); ?>
           </time>
         </article>
